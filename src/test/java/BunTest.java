6f59920e64a369fa2c23f4ae767aa4fff0b30244
Artem Kravtsov <artyomkravts@gmail.com>
2024-10-13 23:09:35 +0300

    added BunTest

--- src/test/java/BunTest.java
+++ src/test/java/BunTest.java
@@ -1,8 +1,22 @@
+import org.junit.Assert;
+import org.junit.Test;
 import praktikum.Bun;
 
 public class BunTest {
 
     private static Bun bun;
+
+    @Test
+    public void testBunGetName() {
+        bun = new Bun("New bun!", 24.6f);
+        Assert.assertEquals("New bun!", bun.getName());
+    }
+
+    @Test
+    public void testBunGetPrice() {
+        bun = new Bun("New bun!", 24.6f);
+        Assert.assertEquals(24.6f, bun.getPrice(), 0);
+    }
 
 
 }
